7203674031361d0993743cccedbfeaaf7682b45b
Samuel Jonathan <jonathandinesh@gmail.com>
2021-11-06 10:35:45 +0530

    added animation to hamburger

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -21,29 +21,25 @@ const MobileNav = () => {
     <div className="sm:hidden">
       <button
         type="button"
-        className="w-8 h-8 ml-1 mr-1 rounded"
+        className="w-10 h-10 ml-1 mr-1 rounded flex cursor-pointer bg-transparent"
         aria-label="Toggle Menu"
         onClick={onToggleNav}
       >
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-gray-900 dark:text-gray-100"
+          viewBox="0 0 100 100"
+          className={`w-10 h-10 ham ${
+            navShow ? 'active' : ''
+          } hamRotate text-gray-900 dark:text-gray-100`}
         >
-          {navShow ? (
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            />
-          ) : (
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          )}
+          <path
+            className="line top"
+            d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
+          />
+          <path className="line middle" d="m 30,50 h 40" />
+          <path
+            className="line bottom"
+            d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
+          />
         </svg>
       </button>
       <div
